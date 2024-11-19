d5740b5cc657f399990a763db75252fd2ea7f2d6
Kacper Kaczmarski <kkaczmarski.npg@gmail.com>
2024-11-19 16:20:08 +0100

    utils for opening dicom and nifti files

---
+++ utils.py
@@ -0,0 +1,24 @@
+import io
+import zipfile
+import pydicom
+import numpy as np
+import SimpleITK as sitk
+from natsort import natsorted
+
+def load_dicom(zip_file_path):
+    slices = []
+    with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+        for file in natsorted(zip_ref.namelist()):
+            if file.endswith('.dcm'):
+                dcm_file = io.BytesIO(zip_ref.read(file))
+                ds = pydicom.dcmread(dcm_file)
+                slices.append(ds.pixel_array)
+    return np.array(slices)
+
+
+def load_nifti(file_path):
+    mask_image = sitk.ReadImage(file_path) 
+    mask_array = sitk.GetArrayFromImage(mask_image)
+
+    binary_mask = (mask_array == 4).astype(np.uint8)
+    return binary_mask
