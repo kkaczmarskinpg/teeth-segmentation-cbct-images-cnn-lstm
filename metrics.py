38b549844c064d022fd2f34a479c7c388af601b8
Kacper Kaczmarski <kkaczmarski.npg@gmail.com>
2024-11-24 22:46:38 +0100

    adjusted metrics

--- metrics.py
+++ metrics.py
@@ -1,18 +1,15 @@
 import tensorflow as tf
 
 def dice_coefficient(y_true, y_pred, smooth=1e-6):
-    
-    y_true_f = tf.flatten(y_true)
-    y_pred_f = tf.flatten(y_pred)
-    intersection = tf.dot(y_true, tf.transpose(y_pred))
-    union = tf.dot(y_true,tf.transpose(y_true))+tf.dot(y_pred,tf.transpose(y_pred))
+    y_true_f = tf.reshape(y_true, [-1])  
+    y_pred_f = tf.reshape(y_pred, [-1])  
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f)
     return (2. * intersection + smooth) / (union + smooth)
 
 def jaccard_index(y_true, y_pred, smooth=1e-6):
-
-    y_true_f = tf.flatten(y_true)
-    y_pred_f = tf.flatten(y_pred)
-    intersection = tf.dot(y_true, tf.transpose(y_pred))
-    union = tf.dot(y_true,tf.transpose(y_true))+tf.dot(y_pred,tf.transpose(y_pred))
+    y_true_f = tf.reshape(y_true, [-1])  
+    y_pred_f = tf.reshape(y_pred, [-1])  
+    intersection = tf.reduce_sum(y_true_f * y_pred_f)
+    union = tf.reduce_sum(y_true_f) + tf.reduce_sum(y_pred_f) - intersection
     return (intersection + smooth) / (union + smooth)
-
